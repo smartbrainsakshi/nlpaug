5dce0b58c2997035f67bc5cd4c45e9dc68ecffd3
dPSudocoder <princesharzeel.10@gmail.com>
2021-07-14 21:02:20 +0530

    :zap: aug keyboard and antonym

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import nlpaug
 import nlpaug.augmenter.word as naw
+import nlpaug.augmenter.char as nac
 import nltk
 
 app = Flask(__name__)
@@ -12,11 +13,17 @@ def my_form():
 @app.route('/index', methods=['POST'])
 def my_form_post():
     text = request.form['text']
+    result = []
     if request.form['group1'] == 'Positive':
         aug = naw.SynonymAug(aug_src='wordnet')
+        result = aug.augment(text,n=2)
     else:
+        kaug = nac.KeyboardAug()
+        result.append(kaug.augment(text,n=1))
         aug = naw.AntonymAug()
-    result = aug.augment(text,n=2)
+        result.append(aug.augment(text,n=1))
+        result.append(kaug.augment(result[1],n=1))
+
     return render_template('index.html', result=result, input_text=text)
 
 
