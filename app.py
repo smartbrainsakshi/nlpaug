37ed9235321cc4781b392b8cdbe85c1fe46b953c
dPSudocoder <princesharzeel.10@gmail.com>
2021-07-14 13:49:47 +0530

    :zap: augmented texts

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import nlpaug
+import nlpaug.augmenter.word as naw
+import nltk
+
+app = Flask(__name__)
+
+@app.route('/index')
+def my_form():
+    return render_template('index.html',input_text="")
+
+@app.route('/index', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    if request.form['group1'] == 'Positive':
+        aug = naw.SynonymAug(aug_src='wordnet')
+    else:
+        aug = naw.AntonymAug()
+    result = aug.augment(text,n=2)
+    return render_template('index.html', result=result, input_text=text)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
