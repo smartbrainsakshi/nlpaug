67419cc023be1b099ae626860affdb300082f83f
dPSudocoder <princesharzeel.10@gmail.com>
2021-07-16 17:51:03 +0530

    :zap: augmenter

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template
 import nlpaug
+import random
+import string
 import nlpaug.augmenter.word as naw
 import nlpaug.augmenter.char as nac
 import nltk
-from textaugment import EDA
+from textaugment import EDA, Wordnet
+import emoji
 
 app = Flask(__name__)
 
@@ -17,20 +20,76 @@ def my_form_post():
     result = []
     if request.form['group1'] == 'Positive':
         t = EDA()
+        t1 = Wordnet()
         result = [
             t.random_swap(text),
             t.random_deletion(text, p=0.3),
             t.random_insertion(text),
             naw.SynonymAug(aug_src='wordnet').augment(text, n=1),
+            t1.augment(text),
+            en.add_synonyms(text, p=1.0),
+            nac.OcrAug().augment(text, n=1),
+            nac.KeyboardAug().augment(text, n=1),
+            nac.RandomCharAug('insert').augment(text, n=1),
+            nac.RandomCharAug('swap').augment(text, n=1),
+            nac.RandomCharAug('delete').augment(text, n=1),
         ]
+
     else:
-        aug = nac.OcrAug()
-        result = []
-        result.append(aug.augment(text, n=1))
-        aug = nac.KeyboardAug()
-        result.append(aug.augment(text, n=1))
+
+        t = EDA()
+        words = text.split(" ")
+        half_txt = " ".join(words[:int(len(words)/2)])
+        rem_txt = " ".join(words[int(len(words)/2):])
+        n = int(len(words)/2)
+        
+        
+        #0. replace with emojis
+        result.append(text_to_emoji(text))
+        #1. make antonym of whole text
+        result.append(naw.AntonymAug().augment(text, n=1))
+        #2. insert n words in the half sentence, where n = half of size of sentence
+        rand_index = random.randint(0,n)
+        result.append(t.random_insertion(sentence=words[rand_index], n=n)+ " " +rem_txt)
+        #3. make antonym of whole text and insert a special character at any position
+        result.append(get_special_char(t, words, len(words)))
+        #4. swap half of the sentence
+        result.append(t.random_swap(half_txt)+ " " +rem_txt)
+        #5. make half sentence antonym
+        result.append(naw.AntonymAug().augment(half_txt, n=1)+ " " +rem_txt)
+        #6. insert one random word in half text
+        result.append(t.random_insertion(half_txt)+ " " +rem_txt)
+        #7. antonym of half and insert random char in another half
+        result.append(naw.AntonymAug().augment(half_txt, n=1) + " " + nac.RandomCharAug('insert').augment(rem_txt, n=1))
+        #8. antonym of half and swap char in other half
+        result.append(naw.AntonymAug().augment(rem_txt, n=1) + " " + nac.RandomCharAug('swap').augment(half_txt, n=1))
+        #9. antonym of half and swap word in another half
+        result.append(naw.AntonymAug().augment(rem_txt, n=1)+ " " +t.random_swap(half_txt),)
 
     return render_template('index.html', result=result, input_text=text)
+
+
+def get_special_char(t, words, n):
+
+    s = " ".join(naw.AntonymAug().augment(words, n=1))
+    index = random.randint(0,n)
+    return s[:index] + random.choice(string.punctuation)  + s[index + 1:]
+
+
+def text_to_emoji(text):
+    text = text.replace(",","").replace(".","")
+    new_sentence = " ".join([":"+s+":" for s in text.split(" ")])
+    print(new_sentence)
+    emojized =  emoji.emojize(new_sentence, use_aliases=True).split(" ")
+
+    sent = []
+    for each in emojized:
+        if each in emoji.UNICODE_EMOJI['en']:
+            sent.append(each)
+        else:
+            sent.append(each.replace(":", ""))
+    print(sent)
+    return " ".join(sent)
 
 
 if __name__ == "__main__":
