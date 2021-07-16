baf2d20cbd6d1a78149d1ef4eba6e2e0283ca5ec
dPSudocoder <princesharzeel.10@gmail.com>
2021-07-16 13:21:52 +0530

    :zap: postive aug

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import nlpaug
 import nlpaug.augmenter.word as naw
 import nlpaug.augmenter.char as nac
 import nltk
+from textaugment import EDA
 
 app = Flask(__name__)
 
@@ -15,14 +16,18 @@ def my_form_post():
     text = request.form['text']
     result = []
     if request.form['group1'] == 'Positive':
-        aug = naw.SynonymAug(aug_src='wordnet')
-        result = aug.augment(text,n=2)
+        t = EDA()
+        result = [
+            t.random_swap("John is going to town"),
+            t.random_deletion("John is going to town", p=0.2),
+            t.random_insertion("John is going to town"),
+        ]
     else:
-        aug = naw.AntonymAug()
-        result.extend(aug.augment(text,n=5))
-        kaug = nac.KeyboardAug()
-        result.append(kaug.augment(text,n=1))
-        result.append(kaug.augment(result[5],n=1))
+        aug = nac.OcrAug()
+        result = []
+        result.append(aug.augment(text, n=1))
+        aug = nac.KeyboardAug()
+        result.append(aug.augment(text, n=1))
 
     return render_template('index.html', result=result, input_text=text)
 
