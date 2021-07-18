64143609218b1672d65cb768166940ed66ea4fe0
dPSudocoder <princesharzeel.10@gmail.com>
2021-07-18 23:15:19 +0530

    :zap: dropdonw

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ import nlpaug.augmenter.char as nac
 import nltk
 from textaugment import EDA, Wordnet
 import emoji
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testcase.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
 positive_options = [
     "Random word swap",
@@ -41,53 +46,53 @@ def my_form():
 def my_form_post():
     text = request.form['text']
     result = []
-    # if request.form['group1'] == 'Positive':
-    pos_logic = request.form.get('pos-logic')
-    neg_logic = request.form.get('neg-logic')
-    if pos_logic:
-        result = [[pos_logic, apply_pos_logic(pos_logic, text)]]
-    elif neg_logic:
-        result = evaluate_negative_augmentation(text, neg_logic)
+    pos_logics = request.form.getlist('pos-logic')
+    neg_logics = request.form.getlist('neg-logic')
 
-    # else:
-    # result = evaluate_negative_augmentation(text)
+    if pos_logics:
+        t = EDA()
+        for logic in pos_logics:
+            result.append([logic, apply_pos_logic(logic, text, t)])
+        
+    elif neg_logics:
+        t = EDA()
+        words = text.split(" ")
+        half_txt = " ".join(words[:int(len(words)/2)])
+        rem_txt = " ".join(words[int(len(words)/2):])
+        n = int(len(words)/2)
+        for logic in neg_logics:
+            result.append(evaluate_negative_augmentation(text, logic, t, half_txt, rem_txt, n, words))
 
     return render_template('index.html', result=result, input_text=text, positive_options=positive_options, negative_options=negative_options)
 
 
 # helper functions are below
 
-def evaluate_negative_augmentation(text, neg_logic):
-    t = EDA()
-    words = text.split(" ")
-    half_txt = " ".join(words[:int(len(words)/2)])
-    rem_txt = " ".join(words[int(len(words)/2):])
-    n = int(len(words)/2)
-    result = []
+def evaluate_negative_augmentation(text, neg_logic, t, half_txt, rem_txt, n, words):
+
 
     #0. replace with emojis
     if neg_logic == "Text to emoji":
-        result.append(["Text to emoji", text_to_emoji(text)])
+        return ["Text to emoji", text_to_emoji(text)]
     #1. make antonym of whole text
     elif neg_logic == "Antonym of text":
-        result.append(["Antonym of text", naw.AntonymAug().augment(text, n=1)])
+        return ["Antonym of text", naw.AntonymAug().augment(text, n=1)]
     #2. insert n words in the half sentence, where n = half of size of sentence
     elif neg_logic == "Insert sentence":
         try:
             rand_index = random.randint(0,n)
-            result.append(["Insert sentence", t.random_insertion(sentence=words[rand_index], n=n)+ " " +rem_txt])
+            return ["Insert sentence", t.random_insertion(sentence=words[rand_index], n=n)+ " " +rem_txt]
         except:
             pass
     #3. make antonym of whole text and insert a special character at any position
     elif neg_logic == "Special character insertion":
-        result.append(["Special character insertion", get_with_special_char(text)])
+        return ["Special character insertion", get_with_special_char(text)]
     #4. swap half of the sentence
     elif neg_logic == "Swap in the sentence":
-        result.append(["Swap in the sentence", t.random_swap(half_txt)+ " " +rem_txt])
+        return ["Swap in the sentence", t.random_swap(half_txt)+ " " +rem_txt]
     #5. insert one random word in half text
     elif neg_logic == "Sentence insertion":
-        result.append(["Sentence insertion", t.random_insertion(half_txt)+ " " +rem_txt])
-    return result
+        return ["Sentence insertion", t.random_insertion(half_txt)+ " " +rem_txt]
 
 
 def get_with_special_char(text):
@@ -119,8 +124,7 @@ def text_to_emoji(text):
     return " ".join(sent)
 
 
-def apply_pos_logic(logic, text):
-    t = EDA()
+def apply_pos_logic(logic, text, t):
     if logic=="Random word swap": 
         return t.random_swap(text)
     elif logic=="Random word delete":
@@ -134,7 +138,7 @@ def apply_pos_logic(logic, text):
     elif logic=="KeyBoard Augmentation": 
         return nac.KeyboardAug().augment(text, n=1)
     elif logic=="Random Char insert": 
-        a= nac.RandomCharAug('insert').augment(text, n=1)
+        return nac.RandomCharAug('insert').augment(text, n=1)
     elif logic=="Random Char swap": 
         return nac.RandomCharAug('swap').augment(text, n=1)
     elif logic=="Random Char delete": 
